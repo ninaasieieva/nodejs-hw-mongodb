3121cb22e00d8894d0ddfdc439e41f81000a80e5
ninaasieieva <160421199+ninaasieieva@users.noreply.github.com>
2024-10-20 09:50:04 +0300

    1

---
+++ src/db/models/contacts.js
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String },
+    isFavourite: { type: Boolean, default: false },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
+  },
+  { timestamps: true },
+);
+
+export const ContactsCollection = model('Contact', contactSchema);
